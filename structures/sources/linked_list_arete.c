fa6084518e938386220c08ba3237349eb2480792
Ununhexium293 <quentin.lv04@gmail.com>
2025-02-17 11:42:08 +0100

    Adding the function edge_list_t_add to linked_list_arete.c

--- structures/sources/linked_list_arete.c
+++ structures/sources/linked_list_arete.c
@@ -2,10 +2,12 @@
 
 //#define DEBUG
 
-edge_t *edge_t_init(int node, int color, int length){
+edge_t *edge_t_init(int node, int color, int length)
+{
     edge_t *edge = malloc(sizeof(edge_t));
 
-    if (edge == NULL){
+    if (edge == NULL)
+    {
         printf("Allocation of size edge_t failed.\n");
         exit(EXIT_FAILURE);
     }
@@ -15,4 +17,22 @@ edge_t *edge_t_init(int node, int color, int length){
     edge -> length = length;
 
     return edge;
+}
+
+edge_list_t *edge_list_t_add(edge_list_t *list, int node, int color, int length)
+{
+    edge_t *edge = edge_t_init(node, color, length);
+
+    edge_list_t *edge_list = malloc(sizeof(edge_list_t));
+
+    if (edge_list == NULL)
+    {
+        printf("Allocation of size edge_list_t failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    edge_list -> edge = edge;
+    edge_list -> next = list;
+
+    return edge_list;
 }
