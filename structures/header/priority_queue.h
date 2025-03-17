66a434d02871daf0e431877b3a0d163548041cd3
Ununhexium293 <quentin.lv04@gmail.com>
2025-03-17 21:57:14 +0100

    creation of priority_queue.h and planning of implementation to make

---
+++ structures/header/priority_queue.h
@@ -0,0 +1,20 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+typedef struct p_queue_{
+    int node;
+    float priority;
+    struct p_queue_ tail;
+} p_queue_t;
+
+/*add an element to the priority queue*/
+void p_queue_t_add(p_queue_t **queue, int node, float priority);
+
+/*remove the head of the queue*/
+void p_queue_t_rm(p_queue_t **queue);
+
+/*free the queue*/
+void p_queue_t_free(p_queue_t *queue);
+
+
+#endif
