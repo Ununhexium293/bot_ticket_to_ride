ff71141902841fb8b2646c65becb6f782aa8ac39
Ununhexium293 <quentin.lv04@gmail.com>
2025-03-24 10:03:53 +0100

    planification for union_find struct

---
+++ structures/header/union_find.h
@@ -0,0 +1,15 @@
+#ifndef UNION_FIND_H
+#define UNION_FIND_H
+
+typedef struct {
+    int *former;
+    int nb_node;
+}union_find_t;
+
+void union_find_t_union(union_find_t *arm, int node_a, int node_b);
+
+int union_find_t_find(union_find_t *arm, int node);
+
+void union_find_t_free(union_find_t *arm);
+
+#endif
