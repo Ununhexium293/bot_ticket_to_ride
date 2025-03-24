d117445035175a02605ad5ee61bbfe4f4a533d14
Ununhexium293 <quentin.lv04@gmail.com>
2025-03-24 12:48:55 +0100

    implementation of union_find_t_init

--- structures/sources/union_find.c
+++ structures/sources/union_find.c
@@ -2,6 +2,34 @@
 #include <stdio.h>
 #include "../header/union_find.h"
 
+union_find_t *union_find_t_init(int nb_node)
+{
+    union_find_t *arm = malloc(sizeof(union_find_t));
+
+    if (arm == NULL)
+    {
+        printf("Allocation of size union_find_t failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int *former = malloc(sizeof(int) * nb_node);
+    if (arm == NULL)
+    {
+        printf("Allocation of size int * nb_node failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < nb_node; i++)
+    {
+        former[i] = i;
+    }
+
+    arm -> former = former;
+    arm -> nb_node = nb_node;
+
+    return arm;
+}
+
 int union_find_t_find(union_find_t *arm, int node)
 {
     int current = arm -> former[node];
