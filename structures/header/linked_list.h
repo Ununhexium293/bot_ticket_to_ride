c19f5be98bcb7072d276d82f7112fc459799d74d
Ununhexium293 <quentin.lv04@gmail.com>
2025-03-27 05:19:00 +0100

    added a general implementation of linked list

---
+++ structures/header/linked_list.h
@@ -0,0 +1,27 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+typedef struct linked_list_
+{
+    void *head;
+    struct linked_list_ *tail;
+}linked_list_t;
+
+/*add the head to the list*/
+void linked_list_t_add(linked_list_t **list, void *head);
+
+/*remove one element from the list and free its allocated space*/
+/*does nothing if head isn't in the list*/
+/*  - is_equal is a function that return a non null integer if both its argument are equal, 0 else*/
+/*  - free_head is a function that free the allocated space of the head*/
+void linked_list_t_rm(linked_list_t **list, void *head, int (*is_equal)(void *, void *), void (*free_head)(void *));
+
+/*free the allocated space of list*/
+/*  - free_head is a function that free the allocated space of the head*/
+void linked_list_t_free(linked_list_t *list, void (*free_head)(void *));
+
+/*print the element of the list*/
+/*  - print_head is a function that print the wanted information from head*/
+void linked_list_t_print(linked_list_t *list, void (*print_head)(void *));
+
+#endif
---
+++ structures/sources/linked_list.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../header/linked_list.h"
+
+void linked_list_t_add(linked_list_t **list, void *head)
+{
+    /*protect against dereferencing a NULL pointer*/
+    if (list == NULL)
+    {
+        return;
+    }
+
+    linked_list_t *new_element = malloc(sizeof(linked_list_t));
+    
+    if (new_element == NULL)
+    {
+        printf("Allocation failure in file %s, function %s, line %d\n", __file__, __func__, __line__);
+        fflush(stdout);
+        exit(EXIT_FAILURE);
+    }
+     
+    new_element -> head;
+    new_element -> tail = *list;
+     
+    *list = new_element;
+}
+
+void linked_list_t_rm(linked_list_t **list, void *head, int (*is_equal)(void *, void *), void (*free_head)(void *))
+{
+    /*protect against dereferencing a NULL pointer*/
+    if (list == NULL)
+    {
+        return;
+    }
+
+    linked_list_t *previous = NULL;
+    linked_list_t *current = *list;
+
+    /*Browse through the list intil either we got to the end of the list or we find the head that we seek*/
+    while (current != NULL && !is_equal(head, current -> head))
+    {
+        previous = current;
+        current = current -> tail;
+    }
+
+    /*if current is NULL that mean that we got to the end of the list without finding head*/
+    if (current != NULL)
+    {
+        if (previous == NULL)
+        {
+            *list = current -> tail;
+        }else{
+            previous = current -> tail;
+        }
+
+        free_head(current -> head);
+        free(current);
+    }
+}
+
+void linked_list_t_free(linked_list_t *list, void (*free_head)(void *))
+{
+    while (list != NULL)
+    {
+        linked_list_t *temp = list;
+        list = list -> tail;
+
+        free_head(temp -> head);
+        free(temp);
+    }
+}
+
+void linked_list_t_print(linked_list_t *list, void (*print_head)(void *))
+{
+    while (list != NULL)
+    {
+        print_head(list -> head);
+
+        list = list -> head;
+    }
+}
